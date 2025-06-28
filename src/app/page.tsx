9dd2fdf14dad4f61a19626727782d621ffecce63
Rajdeep Gill <35fear35@gmail.com>
2025-06-28 13:43:46 -0400

    test out background ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -19,12 +22,13 @@ export default function Home() {
     <div className="p-4 max-w-7xl mx-auto">
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "test" })}
+        onClick={() => {
+          invoke.mutate({ value });
+        }}
       >
-        Invoke Background Job
+        Invoke background job
       </Button>
-      <div>OUTPUT:</div>
-      <pre className="mt-4">{invoke.data?.ok}</pre>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
     </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
 import { inngest } from "./client";
+import { Agent, gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert Programmer. Write code to solve the problem below.",
+      model: gemini({ model: "gemini-2.0-flash" }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Solve this problem in python: ${event.data.value}`
+    );
+    console.log(output);
+
+    return { output };
   }
 );
