cb73fb51c300cb9ec4bdb2cff137a57ef930df8d
Rajdeep Gill <35fear35@gmail.com>
2025-06-29 12:04:24 -0400

    create e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
-import { Agent, gemini, createAgent } from "@inngest/agent-kit";
+import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("void-nextjs");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: "summarizer",
       system:
@@ -15,8 +22,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Solve this problem in python: ${event.data.value}`
     );
-    console.log(output);
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
